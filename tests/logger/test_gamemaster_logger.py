fc79bba911ae2debfb356e962c9a797796adfa07
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2026-05-21 23:11:18 -0300

    refactor: test names without numbers
    It's going to make my life easier down the road.

--- tests/logger/test_gamemaster_logger.py
+++ tests/logger/test_gamemaster_logger.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from gamemaster.logger.gaming import get_gamemaster_logger
 
 class TestGameMasterLogger(TestCase):
-    def test_1_returns_the_same_logger(self):
+    def test_returns_the_same_logger(self):
         logger_1 = get_gamemaster_logger()
         logger_2 = get_gamemaster_logger()
 
--- tests/models/test_player.py
+++ tests/models/test_player.py
@@ -4,7 +4,7 @@ from gamemaster.models.player import Player
 
 
 class TestPlayer(TestCase):
-    def test_1_has_attributes_properly_setup(self):
+    def test_has_attributes_properly_setup(self):
         player_name = "John"
         player_discord_id = 12345678
         player = Player(0, player_name, player_discord_id, None, None)
@@ -22,47 +22,47 @@ class TestPlayer(TestCase):
         self.assertIsNone(player.profile_img)
 
 
-    def test_2_should_throw_error_if_wrong_type_of_name(self):
+    def test_should_throw_error_if_wrong_type_of_name(self):
         with self.assertRaises(TypeError):
             Player(0, 10, 1111111, None, None)
 
 
-    def test_3_throw_error_if_empty_name(self):
+    def test_throw_error_if_empty_name(self):
         with self.assertRaises(ValueError):
             Player(0, "", 1111)
 
 
-    def test_4_name_too_short(self):
+    def test_name_too_short(self):
         with self.assertRaises(ValueError):
             Player(0, "MJ", 1234)
 
 
-    def test_5_name_too_long(self):
+    def test_name_too_long(self):
         with self.assertRaises(ValueError):
             Player(0, "ThisIsAVeryLongNmaeThatIsNotValidForThisContext", 1234)
 
 
-    def test_6_default_emoji_is_None(self):
+    def test_default_emoji_is_None(self):
         player = Player(0, "abc", 2222)
 
         self.assertIsNone(player.emoji)
 
 
-    def test_7_emoji_is_wrong_type(self):
+    def test_emoji_is_wrong_type(self):
         with self.assertRaises(TypeError):
             Player(0, "player", 12345678, emoji=[])
 
 
-    def test_8_emoji_is_blank(self):
+    def test_emoji_is_blank(self):
         with self.assertRaises(ValueError):
             Player(0, "player", 12345678, emoji="")
 
 
-    def test_8_emoji_has_more_than_one_char(self):
+    def test_emoji_has_more_than_one_char(self):
         with self.assertRaises(ValueError):
             Player(0, "player", 12345678, emoji="\u2699\u2700")
 
 
-    def test_9_text_is_not_emoji(self):
+    def test_text_is_not_emoji(self):
         with self.assertRaises(ValueError):
             Player(0, "player", 12345678, emoji="A")
