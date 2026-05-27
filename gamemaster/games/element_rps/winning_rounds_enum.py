9691d7dd78b99fba89aa73319b33f4f53a2b4103
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2026-05-27 16:59:22 -0300

    refactor: changed the winning rounds of RPS

--- gamemaster/games/element_rps/winning_rounds_enum.py
+++ gamemaster/games/element_rps/winning_rounds_enum.py
@@ -5,6 +5,7 @@ class WinningRoundsSetting(IntEnum):
     """How many rounds until the game is won."""
 
     ONE_ROUND = 1
+    TWO_ROUNDS = 2
     THREE_ROUNDS = 3
+    FOUR_ROUNDS = 4
     FIVE_ROUNDS = 5
-    SEVEN_ROUNDS = 7
