93d7fbd882f13103658ca98fd7f2ca77f14ccd6e
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2026-07-04 00:26:35 -0300

    test+feat: ChubSweeperGame now distinguishes between dealer and miners

--- gamemaster/games/chubsweeper/chubsweeper_game.py
+++ gamemaster/games/chubsweeper/chubsweeper_game.py
@@ -28,6 +28,7 @@ class ChubSweeperGame(BaseGame[ChubSweeperOptions]):
                  *,
                  options: ChubSweeperOptions):
         super().__init__(bot, host_user, players, options=options)
+        self._dealer, self._miners = self._distinguish_players()
 
 
     @staticmethod
@@ -53,3 +54,43 @@ class ChubSweeperGame(BaseGame[ChubSweeperOptions]):
     @staticmethod
     def maximum_players() -> int:
         return 9
+
+
+    def _distinguish_players(self) -> tuple["Player", list["Player"]]:
+        """Tries to separate the host player from the rest.
+        
+        Raises:
+            ValueError: If, somehow, the host user isn't in the player's list.
+
+        Returns:
+            A tuple where the first element is the host user (a.k.a. the \"Dealer\"), and the
+            second element is a list of the rest of the players (i.e. the \"Miners\").
+        """
+
+        dealer = None
+        miners = []
+        for player in self.players:
+            if player.discord_user_id == self.host_user.id:
+                dealer = player
+            else:
+                miners.append(player)
+
+        if dealer is None:
+            raise ValueError("No dealer detected in this game. "
+                             f"It should be the user with id {self.host_user.id!r}")
+
+        return dealer, miners
+
+
+    @property
+    def dealer(self) -> "Player":
+        """Fetches the host user of this game."""
+
+        return self._dealer
+
+
+    @property
+    def miners(self) -> list["Player"]:
+        """Fetches the players of this game, other than the host."""
+
+        return self._miners
--- tests/games/chubsweeper/test_chubsweeper_game.py
+++ tests/games/chubsweeper/test_chubsweeper_game.py
@@ -11,13 +11,20 @@ from gamemaster.models import Player
 class TestChubSweeper(TestCase):
     def setUp(self):
         gamemaster_mock = Mock(GameMaster)
-        user_mock = Mock(User)
+
+        self.host_id = 123456789
+        user_mock = Mock(User, **{"id": self.host_id})
+        self.dealer_mock = Mock(Player, **{"discord_user_id": self.host_id})
+
         chubsweeper_options_mock = Mock(ChubSweeperOptions)
+
+        self.miners_amount = 5
+        self.miners_mock = [Mock(Player) for _ in range(self.miners_amount)]
 
         self.chubsweeper_game = ChubSweeperGame(
             gamemaster_mock,
             user_mock,
-            [Mock(Player) for _ in range(5)],
+            [self.dealer_mock, *self.miners_mock],
             options=chubsweeper_options_mock
         )
 
@@ -44,3 +51,23 @@ class TestChubSweeper(TestCase):
 
     def test_has_correct_amount_of_maximum_players(self):
         self.assertEqual(self.chubsweeper_game.maximum_players(), 9)
+
+
+    def test_has_dealer(self):
+        self.assertHasAttr(self.chubsweeper_game, "dealer")
+        self.assertEqual(self.chubsweeper_game.dealer, self.dealer_mock)
+
+
+    def test_has_miners(self):
+        self.assertHasAttr(self.chubsweeper_game, "miners")
+        self.assertCountEqual(self.chubsweeper_game.miners, self.miners_mock)
+
+
+    def test_host_player_must_be_present(self):
+        with self.assertRaises(ValueError):
+            ChubSweeperGame(
+                Mock(GameMaster),
+                Mock(User, **{"id": 111111111111}),
+                [Mock(Player) for _ in range(self.miners_amount)],
+                options=Mock(ChubSweeperOptions)
+            )
