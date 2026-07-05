7c6c92b4472f6d4739696cd0e1fea2f9d671c401
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2026-07-05 18:54:36 -0300

    fix: test without proper mock

--- tests/ui/profiles/test_profile_edit_modal.py
+++ tests/ui/profiles/test_profile_edit_modal.py
@@ -16,23 +16,23 @@ class TestProfileEditModal(IsolatedAsyncioTestCase):
         })
         player_repo_mock = Mock(PlayerRepository)
 
-        self.profiled_edit_modal = ProfileEditModal(player_mock, player_repo_mock)
+        self.profile_edit_modal = ProfileEditModal(player_mock, player_repo_mock)
 
-        self.response_mock = AsyncMock(InteractionResponse)
+        self.response_mock = AsyncMock(InteractionResponse, **{"is_done.return_value": False})
         self.interaction_mock = Mock(Interaction, **{"response": self.response_mock})
 
 
     def test_is_initialized_properly(self):
-        self.assertHasAttr(self.profiled_edit_modal, "title")
-        self.assertEqual(self.profiled_edit_modal.title,
+        self.assertHasAttr(self.profile_edit_modal, "title")
+        self.assertEqual(self.profile_edit_modal.title,
                          f" Edit {self.player_name} Profile Details")
 
-        self.assertHasAttr(self.profiled_edit_modal, "timeout")
-        self.assertIsNone(self.profiled_edit_modal.timeout)
+        self.assertHasAttr(self.profile_edit_modal, "timeout")
+        self.assertIsNone(self.profile_edit_modal.timeout)
 
 
     async def test_can_process_submit_callback_with_no_changes(self):
-        await self.profiled_edit_modal.on_submit(self.interaction_mock)
+        await self.profile_edit_modal.on_submit(self.interaction_mock)
 
         self.response_mock.send_message.assert_called_once_with("_No changes were made._",
                                                                 ephemeral=True)
