5b93dd1bb0e0311cabdcfba6818d065339835dae
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2026-07-16 18:21:18 -0300

    feat: detachable views

--- gamemaster/ui/base_view.py
+++ gamemaster/ui/base_view.py
@@ -88,6 +88,18 @@ class BaseView(LayoutView):
         pass
 
 
+    async def pre_detach(self):
+        """Prepares the view right before a detach event.
+
+        This is called right before the view migrates to a new parent message, in case the original
+        needs to be edited.
+
+        The default implementations does nothing, but can be inherited to be changed.
+        """
+
+        pass
+
+
     async def refresh_parent_msg(self,
                                  interaction: Optional["Interaction"]=None,
                                  view: Optional[Self]=None):
@@ -109,17 +121,40 @@ class BaseView(LayoutView):
         await self.parent_msg.edit(view=usable_view)
 
 
-    async def refresh(self, interaction: Optional["Interaction"]=None):
+    async def update_parent_msg(self, interaction: Optional["Interaction"]=None):
+        """Creates a new message and assigns this view to it.
+
+        Args:
+            interaction: The interaction that triggered the response. If not present, it will try
+                         to send a message from previous parent message context.
+        """
+
+        if interaction is not None and not interaction.response.is_done():
+            callback = await interaction.response.send_message(view=self, ephemeral=False)
+            msg = callback.resource
+        else:
+            msg = await self.parent_msg.channel.send(view=self)
+
+        self.parent_msg = msg
+
+
+    async def refresh(self, interaction: Optional["Interaction"]=None, detach: bool=False):
         """Refreshes the elements of this view, then the message itself.
         
         Args:
             interaction: The interaction that triggered the response. If not present, it will try
                          to edit the message as-is.
+            detach: If `True`, migrate this to a new, freshly created parent message.
         """
 
         self.clear_items()
-        await self.reset()
-        await self.refresh_parent_msg(interaction)
+
+        if detach:
+            await self.pre_detach()
+            await self.update_parent_msg(interaction)
+        else:
+            await self.reset()
+            await self.refresh_parent_msg(interaction)
 
 
     async def destroy(self, interaction: Optional["Interaction"]=None):
